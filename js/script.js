61d9971ec5c9cdc659b525559a4eddc9a7c21422
MemeWasTaken <memelazzari02@gmail.com>
2021-12-01 17:12:58 +0100

    JS: crate grid with squares

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 let btnPlay = document.getElementById('btn-play');
 let selectForm = document.getElementById('difficulty-selection');
-
+let gridContainer = document.querySelector('.grid-container');
 // console.log(btnPlay);
 // console.log(selectForm.value);
 
 
 btnPlay.addEventListener('click', function () {
     if (selectForm.value == 0) {
-        console.log(selectForm.value);
-        alert('Funziona il livello Easy');
+        // console.log(selectForm.value);
+        // alert('Funziona il livello Easy');
+        gridContainer.classList.add('size-difficulty-0')
+        for (let i = 0; i < 100; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');        
+            gridContainer.append(square);
+        }
+
     } else if (selectForm.value == 1) {
-        console.log(selectForm.value);
-        alert('Funziona il livello Medium');
+        // console.log(selectForm.value);
+        // alert('Funziona il livello Medium');
+        gridContainer.classList.add('size-difficulty-1')
+        for (let i = 0; i < 81; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');        
+            gridContainer.append(square);
+        }
+
+
     } else if (selectForm.value == 2) {
-        console.log(selectForm.value);
-        alert('Funziona il livello Hard');
+        // console.log(selectForm.value);
+        // alert('Funziona il livello Hard');
+        gridContainer.classList.add('size-difficulty-2')
+        for (let i = 0; i < 49; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');        
+            gridContainer.append(square);
+        }
+
+
     }
+    
 })
